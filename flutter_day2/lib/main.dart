8a60a8a17ef00c1b8ebfcd39f7cd2ecc7f8951f8
Gohxuann <122100966+Gohxuann@users.noreply.github.com>
2024-10-16 12:09:03 +0800

    add fonts

--- flutter_day2/lib/main.dart
+++ flutter_day2/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(
-      home: HomePage(),
+void main() => runApp(MaterialApp(
+      home: const HomePage(),
+      theme: ThemeData(fontFamily: 'Nunito'),
       debugShowCheckedModeBanner: false,
     ));
 
@@ -21,7 +22,7 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     _pageController = PageController(
       initialPage: 0,
-    );
+    )..addListener(_onscroll);
     super.initState();
   }
 
@@ -31,22 +32,34 @@ class _HomePageState extends State<HomePage> {
       body: PageView(
         controller: _pageController,
         children: [
-          makePage(image: 'assets/images/labubu1.jpg'),
-          makePage(image: 'assets/images/labubu2.jpg'),
-          makePage(image: 'assets/images/labubu3.jpg'),
+          makePage(
+              image: 'assets/images/labubu1.jpg',
+              Title: 'LABUBU',
+              description:
+                  'Labubu is a kind-hearted, curious, and optimistic creature with pointed ears, sharp teeth, and a squarish face. Labubu is inspired by the forest spirits of Scandinavia and is part of a universe of characters modeled after elves, fairies, and monsters.'),
+          makePage(
+              image: 'assets/images/labubu2.jpg',
+              Title: 'Zimomo',
+              description:
+                  'Zimomo is fuzzy, long-eared, and sports sharp teeth that they show off when they smile.'),
+          makePage(
+              image: 'assets/images/labubu3.jpg',
+              Title: 'History',
+              description:
+                  'In 2015, Kasing Lung created a fairy world in his three Nordic Fairy Tale picture books, where live The Monsters, which are both good and evil, including Zimomo, Tycoco, Spooky and Pato.'),
         ],
       ),
     );
   }
 }
 
-Widget makePage({image}) {
+Widget makePage({image, Title, description}) {
   return Container(
     decoration: BoxDecoration(
         image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover)),
     child: Container(
       decoration: BoxDecoration(
-        gradient: LinearGradient(begin: Alignment.bottomRight, stops: [
+        gradient: LinearGradient(begin: Alignment.bottomRight, stops: const [
           0.3,
           0.9
         ], colors: [
@@ -55,15 +68,15 @@ Widget makePage({image}) {
         ]),
       ),
       child: Padding(
-        padding: EdgeInsets.all(20), //position of text
+        padding: const EdgeInsets.all(20), //position of text
         child: Column(
             crossAxisAlignment:
                 CrossAxisAlignment.start, //set position of text to start
             children: <Widget>[
-              SizedBox(
+              const SizedBox(
                 height: 40,
               ),
-              Row(
+              const Row(
                 mainAxisAlignment:
                     MainAxisAlignment.end, //set position of text to right end
                 crossAxisAlignment: CrossAxisAlignment
@@ -87,14 +100,89 @@ Widget makePage({image}) {
                   child: Column(
                 mainAxisAlignment:
                     MainAxisAlignment.end, //set position of text to bottom end
-                //crossAxisAlignment: CrossAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Text(
-                    "LABUBU",
-                    style: TextStyle(
+                    Title,
+                    style: const TextStyle(
                         color: Colors.white,
-                        fontSize: 50,
+                        fontSize: 40,
                         fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(right: 3),
+                        child: const Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                          size: 15,
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(right: 3),
+                        child: const Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                          size: 15,
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(right: 3),
+                        child: const Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                          size: 15,
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(right: 3),
+                        child: const Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                          size: 15,
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(right: 5),
+                        child: const Icon(
+                          Icons.star,
+                          color: Colors.grey,
+                          size: 15,
+                        ),
+                      ),
+                      const Text(
+                        "4.0",
+                        style: TextStyle(color: Colors.white70),
+                      ),
+                      const Text(
+                        "(4.5k)",
+                        style: TextStyle(color: Colors.white38, fontSize: 12),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 50),
+                    child: Text(
+                      description,
+                      style: TextStyle(
+                          color: Colors.white.withOpacity(.7),
+                          height: 1.9,
+                          fontSize: 15),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    'READ MORE',
+                    style: TextStyle(color: Colors.white),
                   )
                 ],
               ))
